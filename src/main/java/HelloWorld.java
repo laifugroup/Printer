556bbf8bad208d6745910ada47e5c396b6a6d528
Luck <luck@google.com>
2024-11-15 16:57:11 +0800

    aa

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,16 @@
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.VerticalAlignment;
 import com.itextpdf.text.*;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.text.pdf.draw.LineSeparator;
 
@@ -10,6 +18,7 @@ import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +26,9 @@ import java.util.logging.Logger;
 import static java.lang.System.out;
 // https://blog.csdn.net/yiminghd2861/article/details/115456154
 // itext 简单方便
+//
+// 密码操作等
+// https://blog.csdn.net/xiaofeng_yang/article/details/139902089
 /**
  * 生成一个内容为“Hello World”的pdf文件
  * @author ym
@@ -36,12 +48,17 @@ public class HelloWorld {
             Document document = new Document(rect);
             //Step 2—Get a PdfWriter instance.
             PdfWriter    writer= PdfWriter.getInstance(document, new FileOutputStream(FILE_DIR + "createSamplePDF.pdf"));
-            // 设置密 码为："World"
-//            writer.setEncryption("Hello".getBytes(), "World".getBytes(),
+
+            //用户密码  可以为null或长度为零，等于省略用户密码
+            String userPassword="Hello";
+            //  ownerPassword：所有者密码。如果为null或为空，iText将生成一个随机字符串用作所有者密码。 所有者密码可以查询到用户密码
+            String ownerPassword="World";
+
+//            writer.setEncryption(userPassword.getBytes(StandardCharsets.UTF_8), ownerPassword.getBytes(StandardCharsets.UTF_8),
 //                    PdfWriter.ALLOW_SCREENREADERS,
-//                    PdfWriter.STANDARD_ENCRYPTION_128);
+//                    PdfWriter.ENCRYPTION_AES_256);
             //PDF版本(默认1.4)
-            writer.setPdfVersion(PdfWriter.PDF_VERSION_1_2);
+            writer.setPdfVersion(PdfWriter.PDF_VERSION_1_7);
             //文档属性
             document.addTitle("Title@sample");
             document.addAuthor("Author@rensanning");
@@ -50,11 +67,18 @@ public class HelloWorld {
             document.addCreator("Creator@iText");
 
 
+
+           // Image image =  Image.getInstance(ImageDataFactory.create("D:\\images\\pg2.jpg").getUrl());
+
+
+
+
+
             //BaseFont bfChinese = BaseFont.createFont("STSongStd-Ligth","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
              BaseFont bfChinese = BaseFont.createFont("c://windows//fonts//msyh.ttc,0", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 
             //页边空白
-            document.setMargins(8, 8, 8, 8);
+            document.setMargins(16f, 16f, 16f, 16f);
 
             //Step 3—Open the Document.  
             document.open();
@@ -62,43 +86,162 @@ public class HelloWorld {
             //public Font(     com. itextpdf. text. pdf. BaseFont bf,
             //    float size,
             //    int style )
-
-            Font font=new Font(bfChinese,48.0f);
+//
+            Font font=new Font(bfChinese,48);
             font.setColor(BaseColor.RED);
             font.setStyle(Font.FontStyle.NORMAL.getValue());
-            Paragraph title=new Paragraph("乐园老年社交平台",font);
+            Paragraph title=new Paragraph("纸上谈兵",font);
                 title.setAlignment(Element.ALIGN_CENTER);
-                title.setSpacingAfter(15f);
-                title.setSpacingBefore(15f);
+
             document.add(title);
 
+//            // 获取文档first页面的 PdfCanvas 画布
+//            PdfCanvas canvas = new PdfCanvas(page);
+//            // 设置背景颜色
+//            canvas.saveState()
+//                    .setFillColor(new DeviceRgb(200, 145, 0))
+//                    // 前2个参数代表页面的x和y，此时我定位在页面的左下角
+//                    // 后两个参数代表填充大小
+//                    .rectangle(0, 0, pageSize.getWidth() - 10, pageSize.getHeight() - 10)
+//                    .fill()
+//                    .restoreState();
+
+            // 设置文本的字体、大小、颜色、背景颜色、对齐方式、垂直对其方式
+//            Paragraph paragraph2 = new Paragraph("感谢您阅读蚂蚁小哥的博客！");
+//            paragraph2 .setFont(font);
+//            paragraph2 .setFontSize(18);
+//            paragraph2 .setFontColor(new DeviceRgb(255, 0, 0));
+//            paragraph2 .setBackgroundColor(new DeviceRgb(187, 255, 255));
+//            paragraph2  .setTextAlignment(TextAlignment.CENTER);
+//            paragraph2  .setVerticalAlignment(VerticalAlignment.BOTTOM);
 
             //直线
+
             Paragraph p1 = new Paragraph();
+
             p1.add(new Chunk(new LineSeparator(font)));
-            //p1.add("R");
             document.add(p1);
 
-            //Image对象
-            document.newPage();
-            ClassLoader.class.getResource("111.PNG");
-            String path = Objects.requireNonNull(HelloWorld.class.getClassLoader().getResource("111.PNG")).getPath();
 
-            Image img = Image.getInstance(path);
-            img.setAlignment(Image.LEFT | Image.TEXTWRAP);
-            img.setBorder(Image.BOX);
-            img.setBorderWidth(10);
-            img.setBorderColor(BaseColor.WHITE);
-            img.scaleToFit(200, 200);//大小
+            Font numbersFont=new Font(bfChinese,24);
+            numbersFont.setColor(BaseColor.RED);
+            numbersFont.setStyle(Font.FontStyle.NORMAL.getValue());
+            Paragraph numbersParagraph=new Paragraph("中大站",numbersFont);
+            numbersParagraph.setAlignment(Element.ALIGN_CENTER);
+            numbersParagraph.setSpacingAfter(16f);
+            numbersParagraph.setSpacingBefore(16f);
+            document.add(numbersParagraph);
+
+            Paragraph empty=new Paragraph("\n");
+            empty.setLeading(48);
+            document.add(empty);
+
+            Font contentFont=new Font(bfChinese,20);
+            contentFont.setColor(BaseColor.BLACK);
+            contentFont.setStyle(Font.FontStyle.NORMAL.getValue());
+
+            Paragraph content=new Paragraph("亲爱的谈兵用户：",contentFont);
+
+            document.add(content);
+
+
+            Font content1Font=new Font(bfChinese,18);
+            content1Font.setColor(BaseColor.BLACK);
+            content1Font.setStyle(Font.FontStyle.NORMAL.getValue());
+            Paragraph content1=new Paragraph("恭喜您在中大站地20210124001场次的战斗中获得了胜利。对你表示祝贺!",content1Font);
+                content1.setFirstLineIndent(32);
+                content1.setSpacingBefore(4);
+            document.add(content1);
+
+
+
+            //正文
+
+
+//            ClassLoader.class.getResource("bg_hometop.png");
+//
+//            String path = Objects.requireNonNull(HelloWorld.class.getClassLoader().getResource("bg_hometop.png")).getPath();
+//
+//            //Image对象
+//            Image img = Image.getInstance(path);
+//            img.setPaddingTop(48);
+//
+//            img.setAlignment(Image.ALIGN_CENTER);
+//            img.setBorder(Image.BOX);
+//            img.setBorderWidth(10);
+//            img.setBorderColor(BaseColor.WHITE);
+//            img.scaleToFit(800, 200);//大小
+//            //img.setRotationDegrees(-30);//旋转
+//            document.add(img);
+
+
+            Font font2=new Font(bfChinese,16);
+            font2.setColor(BaseColor.BLACK);
+            font2.setStyle(Font.FontStyle.NORMAL.getValue());
+            Paragraph p2 =new Paragraph("纸上谈兵项目组\n2024年11月01日",font2);
+            p2.setAlignment(Paragraph.ALIGN_RIGHT);
+
+            Paragraph p222 = new Paragraph()
+                    .setFont(timesNewRoman)
+                    .setFontSize(7)
+                    .setFontColor(ColorConstants.GRAY)
+                    .add(author);
+
+            PdfContentByte canvas = writer.getDirectContent();
+            ColumnText.showTextAligned(canvas, Element.ALIGN_BASELINE, p2, 0, 0, 0);
+
+
+//            PdfContentByte canvas = writer.getDirectContent();
+//          /// ColumnText.showTextAligned(canvas, Element.ALIGN_BOTTOM, p2, 0, 100, 0);
+//            canvas.showTextAligned();
+//            canvas.showTextAligned(PdfContentByte.ALIGN_RIGHT,p2,1,1,1);
+            //document.add(p2);
+
+
+            //章
+            String path2 = Objects.requireNonNull(HelloWorld.class.getClassLoader().getResource("111.PNG")).getPath();
+            Image office = Image.getInstance(path2);
+
+            office.setAlignment(Image.ALIGN_BOTTOM|Image.TEXTWRAP);
+            office.setBorder(Image.BOX);
+            office.setBorderWidth(10);
+            office.setBorderColor(BaseColor.WHITE);
+            office.scaleToFit(200, 200);//大小
             //img.setRotationDegrees(-30);//旋转
-            document.add(img);
+            document.add(office);
+
+
+
+
+//            PdfContentByte cb = writer.getDirectContent();
+//            // 添加一些内容以便页面有高度
+//           // document.add(new Paragraph("Sample content"));
+//            // 定位到页面底部
+//            cb.beginText();
+//            cb.setFontAndSize(BaseFont.createFont(), 12);
+//            // 获取页面底部的位置
+//            float pageHeight = document.getPageSize().getHeight();
+//           // cb.goToText(pageHeight - 20, -20); // 偏移量可以根据需要调整
+//
+//            // 添加文本到底部
+//            cb.showText("This is the bottom footer");
+//            cb.endText();
+//            //cb.showTextAligned(PdfContentByte.ALIGN_RIGHT,"1111",1,1,1);
+//            cb.stroke();
+
+
+           // document.newPage();
 
            // document.add(new Paragraph(Document.));
-            document.newPage();
-            writer.setPageEmpty(false);
+            //document.newPage();
+            //writer.setPageEmpty(false);
 
-            document.newPage();
-            document.add(new Paragraph("New page"));
+            //document.newPage();
+            //document.add(new Paragraph("New page"));
+
+
+
+
 
 
 
